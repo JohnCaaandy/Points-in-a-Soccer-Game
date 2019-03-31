dbec3e71ed6ee9d092f11d1a15cbcf979728f1ef
JohnCaaandy <35344853+JohnCaaandy@users.noreply.github.com>
2019-03-31 14:54:31 -0400

    Add files via upload

---
+++ Points_in_a_Soccer_Match.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using System;
+
+public static class Points
+{
+  public static int TotalPoints(string[] games) {
+        int total = 0;
+        foreach (string game in games) {
+          if (game[0] > game[2])
+            total += 3;
+          else if (game[0] == game[2])
+            total += 1;
+        }
+        return total;
+    }
+}
+}
